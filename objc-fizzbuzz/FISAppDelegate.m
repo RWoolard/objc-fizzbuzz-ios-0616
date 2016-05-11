b49186e79b4db38f37096067a3abf59b692663d5
RWoolard <r.woolard456@gmail.com>
2016-05-11 16:58:13 -0400

    FizzBuzzzzzzzzzz

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -13,6 +13,47 @@
      
      */
     
+    // Loop formatted directly into the NSLog()
+    
+    for (NSUInteger x = 1; x <= 100; x++) {
+        NSLog(@"%lu", x);
+    }
+    
+    // No Magic Numbers...
+    
+    NSUInteger start = 1;
+    NSUInteger limit = 100;
+    
+    for (NSUInteger x = start; x <= limit; x++) {
+        NSString *result = @" ";
+        if (x % 3 == 0 && x % 5 == 0) {
+            NSLog(@"FizzBuzz");
+        } else if (x % 5 == 0) {
+            NSLog(@"Buzz");
+        } else if (x % 3 == 0) {
+            NSLog(@"Fizz");
+        } else {
+        result = [NSString stringWithFormat:@"%lu", x];
+        NSLog(@"%@", result);
+     }
+    }
+    
+    // Simpler? A little less wordy...
+    
+    for (NSUInteger x = 1; x <= 100; x++) {
+        if (x % 15 == 0) {
+            NSLog(@"FizzBuzz");
+        } else if (x % 3 == 0) {
+            NSLog (@"Fizz");
+        } else if (x % 5 == 0) {
+            NSLog(@"Buzz");
+        } else {
+        NSLog(@"%lu", x);
+    }
+    }
+    
+    
+    
     // do not alter
     return YES;  //
     ///////////////
